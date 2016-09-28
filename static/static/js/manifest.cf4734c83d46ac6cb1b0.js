a89d2e9026a362a46ece4274f83becfd98afa085
Rijalul Fikri <fikri.desertlion@gmail.com>
2016-09-28 10:04:45 +0700

    New build with a new example

--- static/static/js/manifest.cf4734c83d46ac6cb1b0.js
+++
@@ -1,2 +0,0 @@
-!function(r){function e(t){if(o[t])return o[t].exports;var n=o[t]={exports:{},id:t,loaded:!1};return r[t].call(n.exports,n,n.exports,e),n.loaded=!0,n.exports}var o={};e.m=r,e.c=o,e.p="/"}([]);
-//# sourceMappingURL=manifest.cf4734c83d46ac6cb1b0.js.map
