ea49d87903e1516139c4d56553b97aec5c8ba4a6
Rijalul Fikri <fikri.desertlion@gmail.com>
2017-10-09 18:31:11 +0700

    Design javascript native functionality

--- sample-native/assets/js/main.js
+++ sample-native/assets/js/main.js
@@ -1,15 +1,38 @@
 $(function(){
+  $('.app-sidebar__lists').on('click', 'li', function(){
+    QiscusSDK.core.UI.chatGroup($(this).data('id'));
+  });
   // let's setup options for our widget
   QiscusSDK.core.init({
     AppId: 'DRAGONGO',
+    mode: 'wide',
     options: {
       // When we're success login into qiscus SDK we'll have a 1-on-1 chat to guest2@qiscus.com
       // You can change this to any user you have on your AppId, e.g: contact@your_company.com, etc
-      loginSuccessCallback(data) { QiscusSDK.core.UI.chatTarget('guest2@qiscus.com') },
+      loginSuccessCallback(data) { 
+        // QiscusSDK.core.UI.chatTarget('guest2@qiscus.com') 
+        // Join Qiscus AOV Room
+        QiscusSDK.core.getOrCreateRoomByUniqueId('Qiscus AOV');
+        // Display UI in sidebar
+        $('.app-sidebar__header img').attr('src', QiscusSDK.core.userData.avatar_url);
+        $('.app-sidebar__myinfo div').html(QiscusSDK.core.userData.username);
+        $('.app-sidebar__myinfo span').html('Online');
+
+        // load Room List
+        QiscusSDK.core.loadRoomList()
+          .then(function(rooms) {
+            var lists = '';
+            rooms.forEach(function(room){
+              var avatar = '<img class="room-avatar" src="'+room.avatar+'" width="48" height="48">';
+              lists = lists + '<li data-id="'+room.id+'">'+avatar+'<div><strong>'+room.name+'</strong><span>'+room.last_comment_message+'</span></div></li>'
+            })
+            $('.app-sidebar__lists ul').html(lists);
+          });
+      },
     }
   });
   // login to qiscus
-  QiscusSDK.core.setUser('guest3@qiscus.com', 'password', 'Qiscus Demo');
+  QiscusSDK.core.setUser('fikri@qiscus.com', 'password', 'Rijalul Fikri');
   // render the widget
   QiscusSDK.render();
 })
