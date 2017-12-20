1923750008ce6873f393bb5d64ca3571b05f3444
Afief Sholichuddin <afief@qiscus.co>
2017-12-20 12:22:19 +0700

    Put back into sample app appId

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -1,3 +1,2 @@
-// var SDK_APP_ID = 'sampleapp-65ghcsaysse';
-var SDK_APP_ID = 'qiscuscs-5xm3wgs5fc1p';
-var SDK_DASHBOARD_URL = 'https://qiscuscs-admin.herokuapp.com';
+var SDK_APP_ID = 'sampleapp-65ghcsaysse';
+var SDK_DASHBOARD_URL = 'https://dashboard-sample.herokuapp.com';
